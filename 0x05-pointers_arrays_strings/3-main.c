df4ee815f3cd0ca91aee4456b659f467ed02ffec
Makura <tapiwanigel72@gmail.com>
2023-02-27 14:50:22 -0800

    3-puts

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *str;
+
+	str = "l do not fear computers. l fear te lack of them - lsaac Asimov";
+	_puts(str);
+	return (0);
+}
