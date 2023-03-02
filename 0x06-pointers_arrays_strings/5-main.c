e314deeecd2eae0e360839d3501f134afa19517f
Makura <tapiwanigel72@gmail.com>
2023-03-02 12:07:18 -0800

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int mai(void)
+{
+	char s[] = "Lookup!\n";
+	char *p;
+
+	p = string_toupper(str);
+	printf("%s", ptr);
+	printf("%s", str);
+	return (0);
+}
