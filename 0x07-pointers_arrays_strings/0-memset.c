61ed5d17cddf436f1ff5e785f9d7af21ddc246bc
Makura <tapiwanigel72@gmail.com>
2023-03-06 11:39:01 -0800

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _memset - fill n bytes of memory with aconstant value
+ * @s: poointer to memory area
+ * @b: constant value
+ * @n: number of bytes to fill
+ * Return: memory area
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+	int size = n /* only accepts positive sizes */
+
+	if (size > 0)
+	{
+		int i;
+
+		for (i = 0; i < size; i++)
+			s[i] = b;
+	}
+
+	return (s);
+}
