799a3c9dee9f3bb4349c0396777388b5eb74dc63
Makura <tapiwanigel72@gmail.com>
2023-03-08 13:30:40 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -6,6 +6,22 @@
  * @root: test this root
  * Return: natural square root, or -1 if not natural root
  */
+int find_root(int n, int root)
+{
+	if (root * root > n)
+		return (-1);
+
+	if (root * root == n)
+		return (root);
+
+	return (find_root(n, root + 1));
+}
+
+/**
+ * -sqrt_recursion - find natural square root of n
+ *  @n: n
+ *  Return: natural square root, or -1 if not natural root
+ */
 
 int _sqrt_recursion(int n)
 {
