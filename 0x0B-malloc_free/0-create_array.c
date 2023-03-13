01c0349120793c43340b8da0d983074434a630d0
Makura <tapiwanigel72@gmail.com>
2023-03-13 14:39:07 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size <= 0) /* validate size input */
 		return (NULL);
-	arr = malloc(sizeof(char) *size); /* allocate memory */
+	arr = malloc(sizeof(char) * size); /* allocate memory */
 
 	if (arr == NULL) /* validate memory */
 		return (NULL);
